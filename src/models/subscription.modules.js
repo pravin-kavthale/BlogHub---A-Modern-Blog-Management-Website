a1f98b0c407bc3bcb163e5001ae30db4d66d41b2
pravin-kavthale <kavthalepravin@gmail.com>
2025-02-01 22:27:14 +0530

    add subscription model for user channel subscriptions

---
+++ src/models/subscription.modules.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const subcriptionSchema = new mongoose.Schema(
+  {
+    subscriber: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    channel: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Subcription = mongoose.model("Subscription", subcriptionSchema);
