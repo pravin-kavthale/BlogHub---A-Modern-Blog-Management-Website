f8b32e9349110df91daadf484ebe0ed2bf4ba326
pravin-kavthale <kavthalepravin@gmail.com>
2025-02-09 22:48:58 +0530

    add user route handlers for password change, profile updates, and watch history retrieval

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -4,9 +4,18 @@ import {
   logoutUser,
   registerUser,
   refreshAccessToken,
+  changeUserPassword,
+  updateFullName,
+  updateusername,
+  updateAvatar,
+  updatecoverImage,
+  updateEmail,
+  getUserDetials,
+  getWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middleware/multer.middleware.js";
 import { verifyJwt } from "../middleware/auth.middleware.js";
+import multer from "multer";
 
 const router = Router();
 router.route("/register").post(
@@ -26,4 +35,18 @@ router.route("/register").post(
 router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJwt, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+
+router.route("/change-password").post(verifyJwt, changeUserPassword);
+router.route("/update-fullname").patch(verifyJwt, updateFullName);
+router.route("/update-username").patch(verifyJwt, updateusername);
+router
+  .route("/update-avatar")
+  .patch(verifyJwt, upload.single("avatar"), updateAvatar);
+router
+  .route("/update-cover-image")
+  .patch(verifyJwt, upload.single("coverImage"), updatecoverImage);
+router.route("/update-email").patch(verifyJwt, updateEmail);
+router.route("/c/:username").get(verifyJwt, getUserDetials);
+router.route("/History").get(verifyJwt, getWatchHistory);
+
 export default router;
